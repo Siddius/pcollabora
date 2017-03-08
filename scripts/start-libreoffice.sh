285d254e768a849eea7b5d6c598d3af05c29aaf2
Siddius <jan.weber-wf@web.de>
2017-03-08 11:12:37 +0100

    Update start-libreoffice.sh
    changing more variables to upper case

--- scripts/start-libreoffice.sh
+++ scripts/start-libreoffice.sh
@@ -21,8 +21,8 @@ mv certs/ca/root.crt.pem /etc/loolwsd/ca-chain.cert.pem
 
 # Replace trusted host and set admin username and password
 perl -pi -e "s/localhost<\/host>/${DOMAIN}<\/host>/g" /etc/loolwsd/loolwsd.xml
-perl -pi -e "s/<username desc=\"The username of the admin console. Must be set.\"><\/username>/<username desc=\"The username of the admin console. Must be set.\">${username}<\/username>/" /etc/loolwsd/loolwsd.xml
-perl -pi -e "s/<password desc=\"The password of the admin console. Must be set.\"><\/password>/<password desc=\"The password of the admin console. Must be set.\">${password}<\/password>/g" /etc/loolwsd/loolwsd.xml
+perl -pi -e "s/<username desc=\"The username of the admin console. Must be set.\"><\/username>/<username desc=\"The username of the admin console. Must be set.\">${USERNAME}<\/username>/" /etc/loolwsd/loolwsd.xml
+perl -pi -e "s/<password desc=\"The password of the admin console. Must be set.\"><\/password>/<password desc=\"The password of the admin console. Must be set.\">${PASSWORD}<\/password>/g" /etc/loolwsd/loolwsd.xml
 
 # Start loolwsd
 su -c "/usr/bin/loolwsd --version --o:sys_template_path=/opt/lool/systemplate --o:lo_template_path=/opt/collaboraoffice5.1 --o:child_root_path=/opt/lool/child-roots --o:file_server_root_path=/usr/share/loolwsd" -s /bin/bash lool
